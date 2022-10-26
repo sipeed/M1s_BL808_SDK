948e972026a3dcf275e61cea2a69c57149efa6bb
taorye <taorye@outlook.com>
2022-10-26 15:07:22 +0800

    feat(mathtool): marco ARGMAX_WITHOFF is supported now and macro ARGMAX is based on it

--- components/sipeed/m1s_tools/inc/mathtool/argmax.h
+++ components/sipeed/m1s_tools/inc/mathtool/argmax.h
@@ -1,16 +1,19 @@
 #pragma once
 
-#include <stdint.h>
 #include <assert.h>
+#include <stdint.h>
 
-#define ARGMAX(arr, size)                                         \
-    ({                                                            \
-        uint32_t __retidx = 0;                                    \
-        do {                                                      \
-            assert(size > 0);                                     \
-            for (uint32_t __idx = 1; __idx < size; __idx++) {     \
-                if (arr[__retidx] < arr[__idx]) __retidx = __idx; \
-            }                                                     \
-        } while (0);                                              \
-        (__retidx);                                               \
+#define ARGMAX_WITHOFF(arr, size, off)                                                \
+    ({                                                                                \
+        register uint32_t __retidx = 0;                                               \
+        do {                                                                          \
+            assert((size) > 0);                                                       \
+            assert((off) > 0);                                                        \
+            for (uint32_t __idx = 1; __idx < (size); __idx++) {                       \
+                if ((arr)[__retidx * (off)] < (arr)[__idx * (off)]) __retidx = __idx; \
+            }                                                                         \
+        } while (0);                                                                  \
+        (__retidx);                                                                   \
     })
+
+#define ARGMAX(arr, size) ARGMAX_WITHOFF(arr, size, 1)
