340eebe8a5c328ccba54640a3e752b84eefc3f09
taorye <taorye@outlook.com>
2022-11-01 14:11:44 +0800

    feat(imgtool): add `RGBA88882GRAY`

--- components/sipeed/m1s_tools/inc/imgtool/rgb_cvt.h
+++ components/sipeed/m1s_tools/inc/imgtool/rgb_cvt.h
@@ -47,3 +47,22 @@ static void RGB5652RGBA8888(uint32_t *src, uint16_t *dest, uint16_t w, uint16_t 
         }
     }
 }
+
+static void RGBA88882GRAY(uint32_t *src, uint8_t *dest, uint16_t w, uint16_t h)
+{
+    typedef struct {
+        uint32_t r : 8;
+        uint32_t g : 8;
+        uint32_t b : 8;
+        uint32_t a : 8;
+    } rgba8888_t;
+
+    for (uint16_t i = 0; i < h; i++) {
+        for (uint16_t j = 0; j < w; j++) {
+            dest[i * w + j] = (uint8_t)(((1225 * (uint32_t)((rgba8888_t *)src + i * w + j)->r) +
+                                         (2404 * (uint32_t)((rgba8888_t *)src + i * w + j)->g) +
+                                         (467 * (uint32_t)((rgba8888_t *)src + i * w + j)->b)) >>
+                                        12);
+        }
+    }
+}
